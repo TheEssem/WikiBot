247ac0712d7e2e956718b087a585803c33403565
TheEssem <smswessem@gmail.com>
2019-08-13 20:11:29 -0500

    Added support for multiple wiki urls and replying with the source

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ const client = new Twit({
   access_token_secret: config.accessSecret
 });
 
-const getArticle = () => request(`${config.wikiURL}?action=query&format=json&generator=random&prop=extracts&explaintext&grnnamespace=0&requestid=${Math.floor(Math.random() * 999999).toString()}`)
-  .then(res => res.json())
-  .then(data => parseArticle(data))
-  .catch(error => { throw new Error(error); });
+const getArticle = () => {
+  const source = config.wikiURLs[Math.floor(Math.random() * config.wikiURLs.length)];
+  request(`${source}?action=query&format=json&generator=random&prop=extracts&explaintext&grnnamespace=0&requestid=${Math.floor(Math.random() * 999999).toString()}`)
+    .then(res => res.json())
+    .then(data => parseArticle(data, source))
+    .catch(error => { throw new Error(error); });
+};
 
 function getFirstProp(obj) {
   for (const i in obj) return obj[i];
 }
 
-function parseArticle(data) {
+function parseArticle(data, source) {
   const s = getFirstProp(data.query.pages).extract;
   const headerRegex = /=+ .+? =+/g;
   const regex = /(\b\S+[.?!]["']?\s*)/g;
@@ -28,9 +31,15 @@ function parseArticle(data) {
     const finalString = stringArray[Math.floor(Math.random() * stringArray.length)].substring(0, 280).trim();
     if (config.blacklist.some(r => finalString.includes(r))) {
       if (!finalString.match(/^\d/)) {
-        client.post("statuses/update", { status: finalString }, function(error, data) {
+        client.post("statuses/update", { status: finalString }, function(error, data2, response) {
           if (error) console.error;
-          console.log(`Successfully posted tweet with ID ${data.id}`);
+          console.log(`Successfully posted tweet with ID ${data2.id}`);
+          console.log(`Response: ${response.statusCode} ${response.statusMessage}`);
+          client.post("statuses/update", { status: `Source: ${source.split("api.php")[0]}${getFirstProp(data.query.pages).title.split(" ").join("_")}`, in_reply_to_status_id:  data2.id }, function(error, data3, response2) {
+            if (error) console.error;
+            console.log(`Successfully posted source with ID ${data3.id}`);
+            console.log(`Response: ${response2.statusCode} ${response2.statusMessage}`);
+          });
         });
       } else {
         setTimeout(getArticle, 100);
